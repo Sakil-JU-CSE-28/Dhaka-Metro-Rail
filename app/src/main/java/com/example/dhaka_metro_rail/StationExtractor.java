726b4d5a8f220f82e1ffbc1c62bc26588e89ec80
Sagor <sagor.stu20182@juniv.edu>
2024-02-12 22:46:13 +0600

    Partial Implementation of ViewMetro

---
+++ app/src/main/java/com/example/dhaka_metro_rail/StationExtractor.java
@@ -0,0 +1,65 @@
+package com.example.dhaka_metro_rail;
+
+import android.content.res.Resources;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * This class is for accessing all the stations and metro information from the existing text files by calling respective methods.
+ */
+public class StationExtractor {
+    private Resources resources;
+
+    /**
+     * Constructor for StationExtractor class.
+     *
+     * @param resources The Resources object used to access app resources.
+     */
+    public StationExtractor(Resources resources) {
+        this.resources = resources;
+    }
+
+    /**
+     * Retrieve a set of all the stations from the text file.
+     *
+     * @return A set containing the names of all stations.
+     */
+    public Set<String> getStations(){
+        Set<String> stations = new HashSet<>();
+        InputStream inputStream = resources.openRawResource(R.raw.stations);
+        Scanner scanner = new Scanner(inputStream);
+
+        while (scanner.hasNext()) {
+            String cur = "";
+            String token = scanner.next(); // Tokenizing by words
+            for(int i = 0;i < token.length();i++){
+                if(token.charAt(i) == '-'){
+                    stations.add(cur);
+                    cur = "";
+                }
+                else
+                    cur += token.charAt(i);
+            }
+            stations.add(cur);
+        }
+        return  stations;
+    }
+
+    /**
+     * Retrieve a set of all the metro information from the text file.
+     *
+     * @return A set containing the metro information.
+     */
+    public Set<String> getMetro(){
+        Set<String> metro = new HashSet<>();
+        InputStream inputStream = resources.openRawResource(R.raw.metro);
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNext()) {
+            String token = scanner.next(); // Tokenizing by words
+            metro.add(token);
+        }
+        return metro;
+    }
+}
